556b0cf42054fd791a8d178986503d8e0e96824a
shariff6-school <shariff6@my.yorku.ca>
2022-10-15 12:35:01 -0400

    changed App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          Hello World!
+          Hello im Tamjeed!
         </p>
        
       </header>
